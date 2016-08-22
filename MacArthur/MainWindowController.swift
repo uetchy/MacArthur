b70639f776077d26c0ab73e356a6e6a93645c8a5
Yasuaki Uechi <uetchy@randompaper.co>
2016-08-22 22:06:07 +0900

    chore: indent

--- MacArthur/MainWindowController.swift
+++ MacArthur/MainWindowController.swift
@@ -6,6 +6,15 @@
 //  Copyright © 2016 Yasuaki Uechi. All rights reserved.
 //
 
+import Cocoa
+
 class MainWindowController: NSWindowController {
 
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        let window = self.window!
+//        let storyboard = self.storyboard!
+        window.titleVisibility = .Hidden
+        
+    }
 }
