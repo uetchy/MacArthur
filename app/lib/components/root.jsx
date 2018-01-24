ad994344d9b16f5b4589bceb1022744bb140a228
Yasuaki Uechi <y@uechi.io>
2018-01-24 16:45:13 +0900

    prettier

--- app/lib/components/root.jsx
+++ app/lib/components/root.jsx
@@ -1,45 +1,44 @@
-import React from 'react';
-import { ipcRenderer } from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
-import { Label, View } from 'react-desktop/macOs';
+import React from 'react'
+import { ipcRenderer } from 'electron'
+import { Label, View } from 'react-desktop/macOs'
 
-import Task from './task';
+import Task from './task'
 
 class Root extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      queue: []
-    };
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			queue: [],
+		}
+	}
 
-  componentDidMount() {
-    console.log('start listening');
-    ipcRenderer.on('open-url', (event, gitURL) => {
-      console.log('root open-url:', gitURL);
-      const newQueue = this.state.queue;
-      newQueue.push(<Task key={newQueue.length + 1} url={gitURL} />);
-      this.setState({ queue: newQueue });
-    });
-  }
+	componentDidMount() {
+		console.log('start listening')
+		ipcRenderer.on('open-url', (event, gitURL) => {
+			console.log('root open-url:', gitURL)
+			const newQueue = this.state.queue
+			newQueue.push(<Task key={newQueue.length + 1} url={gitURL} />)
+			this.setState({ queue: newQueue })
+		})
+	}
 
-  render() {
-    const { queue } = this.state;
-    console.log(queue);
-    return (
-      <View
-        background="white"
-        height="100%"
-        horizontalAlignment="center"
-        padding="10px"
-      >
-        {queue.length > 0 ? (
-          queue
-        ) : (
-          <Label textAlign="center">No download queue</Label>
-        )}
-      </View>
-    );
-  }
+	render() {
+		const { queue } = this.state
+		console.log(queue)
+		return (
+			<View
+				background="white"
+				height="100%"
+				horizontalAlignment="center"
+				padding="10px">
+				{queue.length > 0 ? (
+					queue
+				) : (
+					<Label textAlign="center">No download queue</Label>
+				)}
+			</View>
+		)
+	}
 }
 
-export default Root;
+export default Root
--- app/lib/index.jsx
+++ app/lib/index.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { TitleBar, Toolbar, Text } from 'react-desktop/macOs';
+import React from 'react'
+import { render } from 'react-dom'
+import { TitleBar, Toolbar, Text } from 'react-desktop/macOs'
 
-import Root from './components/root';
+import Root from './components/root'
 
 const App = () => (
-  <div>
-    <TitleBar inset>
-      <Toolbar height="38" horizontalAlignment="center">
-        <Text>MacArthur</Text>
-      </Toolbar>
-    </TitleBar>
-    <Root />
-  </div>
-);
+	<div>
+		<TitleBar inset>
+			<Toolbar height="38" horizontalAlignment="center">
+				<Text>MacArthur</Text>
+			</Toolbar>
+		</TitleBar>
+		<Root />
+	</div>
+)
 
-render(<App />, document.querySelector('#root'));
+render(<App />, document.querySelector('#root'))
