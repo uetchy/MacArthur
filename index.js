81d0c9def45f083186c3a1bccca438b0182d1521
Yasuaki Uechi <y@uechi.io>
2018-04-03 19:24:47 +0900

    prettier

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import { TitleBar, Toolbar, Text } from 'react-desktop/macOs'
 import Root from './components/root'
 
 const App = () => (
-	<div>
-		<TitleBar inset>
-			<Toolbar height="38" horizontalAlignment="center">
-				<Text>MacArthur</Text>
-			</Toolbar>
-		</TitleBar>
-		<Root />
-	</div>
+  <div>
+    <TitleBar inset>
+      <Toolbar height="38" horizontalAlignment="center">
+        <Text>MacArthur</Text>
+      </Toolbar>
+    </TitleBar>
+    <Root />
+  </div>
 )
 
 render(<App />, document.querySelector('#root'))
